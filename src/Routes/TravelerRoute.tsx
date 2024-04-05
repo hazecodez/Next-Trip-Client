cf90b8dacf3fee82f9b32843140bc6a3b15bbe6a
hazecodez <muhammedhashir298@gmail.com>
2024-04-05 23:24:33 +0530

    Otp Ui

--- src/Routes/TravelerRoute.tsx
+++ src/Routes/TravelerRoute.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Login from "../Pages/Traveler/LoginPage";
 import SignUp from "../Pages/Traveler/SignUpPage";
 import Home from "../Pages/Traveler/HomePage";
+import OtpPage from "../Pages/Traveler/OtpPage";
 
 export default function TravelerRoute() {
   return (
@@ -9,6 +10,7 @@ export default function TravelerRoute() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/otpPage" element={<OtpPage/>} />
     </Routes>
   );
 }
