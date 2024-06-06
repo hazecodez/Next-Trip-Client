56cfebc91192bdf1fd744d504d026131d50e5e95
hazecodez <muhammedhashir298@gmail.com>
2024-06-07 00:21:45 +0530

    deployment

--- src/Middlewares/Admin/IsLoggedIn.tsx
+++ src/Middlewares/Admin/IsLoggedIn.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Navigate } from "react-router-dom";
+// import { Navigate } from "react-router-dom";
 
 interface MiddlewareProps {
   children: React.ReactNode;
@@ -7,12 +7,13 @@ interface MiddlewareProps {
 
 const IsLoggedIn: React.FC<MiddlewareProps> = ({ children }) => {
   const logged = localStorage.getItem("adminToken")
-
-  if (logged) {
-    return <>{children}</>;
-  } else {
-    return <Navigate to="/admin/login" />;
-  }
+  console.log("Logged aanallo",logged);
+  return <>{children}</>;
+  // if (logged) {
+  //   return <>{children}</>;
+  // } else {
+  //   return <Navigate to="/admin/login" />;
+  // }
 };
 
 export default IsLoggedIn;
