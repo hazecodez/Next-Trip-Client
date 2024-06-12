98fc5421fadafa33c294827459c649f99d7a7b3a
hazecodez <muhammedhashir298@gmail.com>
2024-06-12 15:03:58 +0530

    Account tab ui issue

--- src/Components/Common/AccountTab.tsx
+++ src/Components/Common/AccountTab.tsx
@@ -17,7 +17,7 @@ export default function AccountTab({ tabNumber }: propTypes) {
               type="button"
               className={`${
                 activeTab === "1" ? "dark:bg-gray-800 text-white" : "text-black"
-              } hover:text-gray-800  dark:text-white py-3 px-4 inline-flex items-center gap-x-2 bg-transparent text-sm font-medium text-center text-gray-800  rounded-full  disabled:opacity-50 disabled:pointer-events-none dark:hover:text-dark-800 `}
+              }  py-3 px-4 inline-flex items-center gap-x-2 bg-transparent text-sm font-medium text-center  rounded-full  disabled:opacity-50 disabled:pointer-events-none  `}
               role="tab"
               onClick={() => {
                 navigate("/login");
@@ -28,9 +28,9 @@ export default function AccountTab({ tabNumber }: propTypes) {
             </button>
             <button
               type="button"
-              className={` text-gray-800 hover:text-gray-800 ${
-                activeTab === "2" ? "bg-[#C63D3F] text-white" : "bg-transparent"
-              } dark:text-white py-3 px-4 inline-flex items-center gap-x-2  text-sm font-medium text-center  rounded-full hover:text-neutral-content disabled:opacity-50 disabled:pointer-events-none dark:hover:text-gray-800 `}
+              className={` text-gray-800  ${
+                activeTab === "2" ? "bg-[#C63D3F] text-white" : "bg-transparent text-black"
+              }  py-3 px-4 inline-flex items-center gap-x-2  text-sm font-medium text-center  rounded-full disabled:opacity-50 disabled:pointer-events-none `}
               role="tab"
               onClick={() => {
                 navigate("/host/login");
